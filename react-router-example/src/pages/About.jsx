28b3edc1108e782b525096212d3d4fb7940e5fd9
jjongmin <ljm920604@gmail.com>
2022-04-03 08:29:35 +0900

    react-router query string

--- react-router-example/src/pages/About.jsx
+++ react-router-example/src/pages/About.jsx
@@ -1,3 +1,16 @@
-export default function About() {
-  return <div>About 페이지입니다.</div>;
+import { useSearchParams } from "react-router-dom";
+
+export default function About(props) {
+  console.log(props);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchName = searchParams.get("name");
+  console.log(searchParams);
+  console.log(searchName);
+  return (
+    <div>
+      <h2>About 페이지입니다.</h2>
+      {searchName && <p>name은 {searchName}입니다</p>}
+    </div>
+  );
 }
